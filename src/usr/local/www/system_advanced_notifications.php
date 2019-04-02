9bf46de1273495a4ceeb10a56207a54a01365e5b
stzh <steven.zhou@nokia-sbell.com>
2019-04-02 23:23:53 +0800

    Update system_advanced_notifications.php

--- src/usr/local/www/system_advanced_notifications.php
+++ src/usr/local/www/system_advanced_notifications.php
@@ -52,7 +52,7 @@ if ($config['notifications']['growl']['notification_name']) {
 if ($config['notifications']['growl']['name']) {
 	$pconfig['name'] = $config['notifications']['growl']['name'];
 } else {
-  $pconfig['name'] = 'pfSense-Growl';
+  $pconfig['name'] = 'HHFW-Growl';
 }
 
 
@@ -354,7 +354,7 @@ $section->addInput(new Form_Input(
 	'Registration Name',
 	'text',
 	$pconfig['name'],
-	['placeholder' => 'pfSense-Growl']
+	['placeholder' => 'HHFW-Growl']
 ))->setHelp('Enter the name to register with the Growl server.');
 
 $section->addInput(new Form_Input(
