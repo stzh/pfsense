5a6c1e8f0b1e841b01c40f9490dc7c781059eada
stzh <steven.zhou@nokia-sbell.com>
2019-04-08 20:53:09 +0800

    Update system.php

--- src/usr/local/www/system.php
+++ src/usr/local/www/system.php
@@ -457,7 +457,7 @@ $section->addInput(new Form_Input(
 	'*Hostname',
 	'text',
 	$pconfig['hostname'],
-	['placeholder' => 'pfSense']
+	['placeholder' => 'HHFW']
 ))->setHelp('Name of the firewall host, without domain part');
 
 $section->addInput(new Form_Input(
@@ -660,7 +660,7 @@ $csswarning = sprintf(gettext("%sUser-created themes are unsupported, use at you
 events.push(function() {
 
 	function setThemeWarning() {
-		if ($('#webguicss').val().startsWith("pfSense")) {
+		if ($('#webguicss').val().startsWith("HHFW")) {
 			$('#csstxt').html("").addClass("text-default");
 		} else {
 			$('#csstxt').html("<?=$csswarning?>").addClass("text-danger");
