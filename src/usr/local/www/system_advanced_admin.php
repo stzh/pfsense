1ec9112819e6b77fab027da15912f661edd8999c
stzh <steven.zhou@nokia-sbell.com>
2019-04-13 10:59:26 +0800

    Update system_advanced_admin.php

--- src/usr/local/www/system_advanced_admin.php
+++ src/usr/local/www/system_advanced_admin.php
@@ -704,7 +704,7 @@ if (!$g['enableserial_force'] && !$g['primaryconsole_force']) {
 			'video' => gettext('VGA Console'),
 		)
 	))->setHelp('Select the preferred console if multiple consoles are present. '.
-		'The preferred console will show pfSense boot script output. All consoles '.
+		'The preferred console will show HHFW boot script output. All consoles '.
 		'display OS boot messages, console messages, and the console menu.');
 }
 
